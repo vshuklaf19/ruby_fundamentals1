7776b4e345e3dc5925d2a3780692912965621c7c
Vivek Shukla <vivek.f19@gmail.com>
2015-01-20 13:45:00 -0500

    Added a simple put message to the code (w1d2 e3)

---
+++ excercise2.rb
@@ -0,0 +1,19 @@
+#Excercise 2 - Question 1
+meal = 55
+
+puts "A good tip would be " + (meal*0.15).to_s
+
+#Excercise 2 - Question 2
+my_string = "Hi-"
+my_integer = 5
+
+puts my_string + my_integer.to_s
+
+#Excercise 2 - Question 3
+x = 45628
+y = 7839
+
+puts "The product is: #{x*y}"
+
+#Excercise 2 - Question 4
+puts (true && false) || (false && true) || !(false && false)
